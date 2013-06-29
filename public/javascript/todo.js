31caf67ff54c99fbb5cb8869d46c70341e311f6e
jasonblanchard <jas.blanchard@gmail.com>
2013-06-29 12:29:28 -0400

    Fixing stuff

--- public/javascript/todo.js
+++ public/javascript/todo.js
@@ -101,7 +101,7 @@ $(function() {
 
         el: $("#todoapp"),
 
-        statsTemplate: _.template($('#stats-template').html())
+        statsTemplate: _.template($('#stats-template').html()),
 
         events: {
             "keypress #new-todo": "createOneEnter",
@@ -124,4 +124,48 @@ $(function() {
             Todos.fetch();
         },
 
+        render: function() {
+            var done = Todos.done().length;
+            var remaining = Todos.remaining.length;
 
+            if (Todos.length) {
+                this.main.show();
+                this.footer.show();
+                this.footer.html(this.StatsTemplate({done: done, remianing: remaining}));
+            } else {
+                this.main.hide();
+                this.footer.hide();
+            }
+
+            this.allCheckbox.checked = !remaining;
+            },
+
+        addOne: function(todo) {
+            var view = new TodoView({model: todo});
+            this.$("#todo-list").append(view.render().el);
+        },
+
+        createOnEnter: function(e) {
+            if (e.keyCode != 13) return;
+            if (!this.input.val()) return;
+
+            Todos.create({title: this.input.val()});
+            this.input.val('');
+        },
+
+        clearCompleted: function() {
+            _.invoke(Todos.done(), 'destroy');
+            return false;
+        },
+
+        toggleAllComplete: function() {
+            var done = this.allCheckbox.checked;
+            Todos.each(function (todo) { todo.save({'done': done}); });
+            },
+
+    });
+
+
+    var App = new AppView
+
+});
